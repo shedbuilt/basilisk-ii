c9a0bc62acc137f24cfaf5e93f21069b5f74631c
Auston Stewart <auston@shedbuilt.net>
2018-03-20 09:25:11 -1000

    Fixing framebuffer rendering issues with the SDL backend

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Allow use of software SDL video surfaces
+patch -Np1 -i "${SHED_PATCHDIR}/allow_sw_sdl_surface.patch"
 cd BasiliskII/src/Unix
 # Forcibly replace ancient automake files
 rm config.guess config.sub
@@ -7,8 +9,12 @@ autoreconf -fiv -I m4 &&
 ./configure --prefix=/usr/local \
             --enable-sdl-video \
             --enable-sdl-audio \
+            --enable-addressing=banks \
             --disable-vosf \
             --disable-jit-compiler \
-            --without-gtk &&
+            --without-mon \
+            --without-esd \
+            --without-gtk \
+            --without-x &&
 make -j $SHED_NUMJOBS &&
 make DESTDIR="$SHED_FAKEROOT" install
