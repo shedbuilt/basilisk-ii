8e0ebff016a8be9673c8b55b1d9a5ee06397b285
Auston Stewart <auston@shedbuilt.net>
2020-12-19 11:00:25 -1000

    Testing latest master commits on blank

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Allow use of software SDL video surfaces
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/allow_sw_sdl_surface.patch"
-cd BasiliskII/src/Unix
+## patch -Np1 -i "${SHED_PKG_PATCH_DIR}/allow_sw_sdl_surface.patch"
+cd BasiliskII/src/Unix &&
 # Forcibly replace ancient automake files
-rm config.guess config.sub
-automake -a
+rm config.guess config.sub &&
+automake -a &&
 autoreconf -fiv -I m4 &&
 ./configure --prefix=/usr/local \
             --enable-sdl-video \
             --enable-sdl-audio \
+            --with-sdl2 \
             --enable-addressing=banks \
             --disable-vosf \
             --disable-jit-compiler \
